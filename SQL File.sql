44e61e347071ddcbbf26d5dbf50a63719a560d22
Sasha <alexander.oga@student.mahidol.edu>
2024-03-28 10:50:27 +0700

    Changed orders, payment, ordered_items, menu, added a total cost for orders

--- SQL File.sql
+++ SQL File.sql
@@ -8,18 +8,24 @@ CREATE TABLE customers (
 CREATE TABLE orders (
     order_id SERIAL PRIMARY KEY,
     customer_id INTEGER REFERENCES customers(customer_id),
-    payment_id INTEGER REFERENCES payment(payment_id),
+    payment_id INTEGER REFERENCES payments(payment_id),
+    total_cost DECIMAL(10, 2),
     delivery_location VARCHAR(200),
     status CHAR(50),
     restaurant_branch INTEGER REFERENCES branches(branch_id),
     order_date DATE
 );
 
-CREATE TABLE order_history (
-
+CREATE TABLE ordered_items (
+    ordered_id SERIAL PRIMARY KEY,
+    order_id INTEGER REFERENCES orders(order_id),
+    customer_id INTEGER REFERENCES customers(customer_id),
+    item_id INTEGER REFERENCES menu(item_id),
+    quantity INTEGER,
+    customizations VARCHAR(50)
 );
 
-CREATE TABLE payment (
+CREATE TABLE payments (
     payment_id SERIAL PRIMARY KEY,
     payment_method CHAR(50),
     payment_credentials VARCHAR(100)
@@ -27,10 +33,18 @@ CREATE TABLE payment (
 
 CREATE TABLE menu (
     item_id SERIAL PRIMARY KEY,
-
+    item_cost INTEGER
 );
 
 CREATE TABLE branches (
     branch_id SERIAL PRIMARY KEY,
-
+    branch_location VARCHAR(200)
 );
+
+UPDATE orders o
+SET total_cost = (
+    SELECT SUM(oi.quantity * m.item_cost)
+    FROM ordered_items oi
+    JOIN menu m on oi.item_id = m.item_id
+    WHERE oi.order_id = o.order_id
+    )
